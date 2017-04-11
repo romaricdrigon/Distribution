791fdfc3744d2ef5de3c07ce7e41470558cd51a7
levis0045 <Levis0045@users.noreply.github.com>
2017-04-11 10:39:12 +0200

    First commit

---
+++ plugin/lexicon/ClarolineLexiconBundle.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\LexiconBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+use Claroline\MessageBundle\Installation\AdditionalInstaller;
+
+class ClarolineLexiconBundle extends PluginBundle
+{
+    public function getConfiguration($environment)
+    {
+        $config = new ConfigurationBuilder();
+
+        return $config->addRoutingResource(__DIR__.'/Resources/config/routing.yml', null, 'lexicon');
+    }
+
+    public function getAdditionalInstaller()
+    {
+        return new AdditionalInstaller();
+    }
+
+    public function hasMigrations()
+    {
+        return true;
+    }
+
+    public function getRequiredFixturesDirectory($environment)
+    {
+        return 'DataFixtures';
+    }
+}
